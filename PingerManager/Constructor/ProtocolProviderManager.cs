7c258c941621d24c80a947dded9ea6caff89236d
Anatoly Juk™ Sirik <ant7juk@gmail.com>
2018-12-11 00:00:47 +0200

    Добавление ProtocolProviderManager Тесты

---
+++ PingerManager/Constructor/ProtocolProviderManager.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+using PingerManager.Config;
+using PingerManager.Logging;
+
+namespace PingerManager.Constructor
+{
+    public class ProtocolProviderManager : IProtocolProviderManager
+    {
+        private readonly ILogger _logger;
+        private readonly ServiceProvider _serviceProvider;
+
+        public ProtocolProviderManager(ILogger logger)
+        {
+            _logger = logger;
+            _serviceProvider = PingerServiceProvider.ServiceProvider;
+        }
+
+        public IProtocolProvider GetProvider(ConfigEntity configEntity)
+        {
+            var protocolProviders = _serviceProvider.GetServices<IProtocolProvider>().ToList();
+            try
+            {
+                switch (configEntity.Protocol)
+                {
+                    case "ICMP":
+                        return protocolProviders.First(o => o.GetType() == typeof(IcmpPing));
+                    case "TCP":
+                        return protocolProviders.First(o => o.GetType() == typeof(TcpPing));
+                    case "HTTP":
+                        return protocolProviders.First(o => o.GetType() == typeof(HttpPing));
+                    default:
+                        throw new ArgumentException(DateTime.Now + " " + "Протокол не поддерживается!");
+                }
+            }
+            catch (ArgumentException e)
+            {
+                _logger.Log(new LogParams(MessageType.Error, e.Message, MainLogPath.LogPath));
+                throw;
+            }
+        }
+
+        #region IDisposable
+        private bool _disposedValue;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposedValue)
+            {
+                if (disposing)
+                {
+                    _serviceProvider?.Dispose();
+                }
+                _disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
